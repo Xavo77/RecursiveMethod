5d27ae03b83351565330cf644eb9499d2b110487
JavierNegrete <janedaz@hotmail.com>
2017-09-04 21:49:03 -0500

    Add files via upload

---
+++ Recursive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of a method recursive");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("Introduce one number: ");
+            string n = Console.ReadLine();
+            Console.WriteLine(MethodRecursive(int.Parse(n)));
+            Console.ReadLine();
+        }
+
+        // Example of recursive method. Get the factorial
+        static decimal MethodRecursive(int num)
+        {
+            if (num <= 0 )
+            {
+                return 1;
+            }
+            return num * MethodRecursive(num - 1);
+        }
+    }
+}
